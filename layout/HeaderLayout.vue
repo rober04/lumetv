562a5fe74bb5d6a198371c09d27a4ac18e377a58
Rober04 <roberto.david.adam04@gmail.com>
2025-04-01 20:21:53 +0200

    feat(components): draft header layout

---
+++ layout/HeaderLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+	<header class="header">
+		<nav class="header__nav nav">
+			<div class="nav__site">
+				<img src="" alt="" class="nav__logo" />
+				<p class="nav__title">LumeTV</p>
+				<img src="" alt="" class="nav__menu" />
+			</div>
+			<div class="nav__searchbar searchbar">
+				<input type="text" name="" id="" class="searchbar__text" />
+				<img src="" alt="" class="searchbar__icon" />
+			</div>
+			<div class="nav__user user">
+				<img src="" alt="" class="user__prime" />
+				<button type="button" class="user__login"></button>
+				<button type="button" class="user__register"></button>
+				<img src="" alt="" class="user__profile" />
+			</div>
+		</nav>
+	</header>
+</template>
+
+<style lang="scss" scoped></style>
