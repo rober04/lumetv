93b85e5b94209fe8460ecc6b5c32e0d83953c37e
Rober04 <roberto.david.adam04@gmail.com>
2025-04-03 17:12:45 +0200

    chore(pages): added template for the main page

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
-<template>
-	<div>
-		<NuxtRouteAnnouncer />
-		<NuxtWelcome />
-	</div>
-</template>
+<script setup lang="ts"></script>
+
+<template><NuxtPage></NuxtPage></template>
+
+<style lang="scss" scoped></style>
