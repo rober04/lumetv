db890134e8b708d8ce3f19727052ed3f07b5c969
Rober04 <roberto.david.adam04@gmail.com>
2025-04-07 15:41:24 +0200

    chore(config): changed nuxt and prettier config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { WebSocketServer } from 'ws';
+import tmi from 'tmi.js';
+
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
@@ -23,5 +25,21 @@ export default defineNuxtConfig({
 		'@nuxt/scripts',
 		'@nuxt/test-utils',
 		'@nuxt/ui',
+		'@pinia/nuxt',
 	],
+	runtimeConfig: {
+		twitchUser: process.env.TWITCH_USERNAME,
+		twitchToken: process.env.TWITCH_TOKEN,
+		twitchChannel: process.env.TWITCH_CHANNEL,
+		public: {
+			twitchUser: process.env.TWITCH_USERNAME,
+			twitchChannel: process.env.TWITCH_CHANNEL,
+		},
+	},
+	nitro: {
+		preset: 'node',
+		experimental: {
+			websocket: true,
+		},
+	},
 });
